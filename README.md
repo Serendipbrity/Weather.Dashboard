783accc6385d87da28030e93ac7bb81747eb2be8
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-08 09:49:48 -0400

    setItem

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Weather.Dashboard
+Get the current weather, and the next 5 days, when you look up  a city.
--- assets/js/script.js
+++ assets/js/script.js
@@ -18,25 +18,29 @@ function getInfo(e) {
     // the users input
     var city = cityInput.value
   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2ff41d70607b09fa349823714cb91710`;
-  
+  // store city input value (city)
+  localStorage.setItem("city",JSON.stringify(city));
+  var stored = JSON.parse(localStorage.getItem("city"));
+  cityName.append(stored);
 
   fetch(apiUrl)
     .then(response => response.json())
-
     .then(data => {
-      console.log(data.main.temp);
       var humidity = (data.main.humidity);
       // api temp is in kelvin 
       var celTemp = (data.main.temp);
       // convert kelvin to fahrenheit
       var fTemp = Math.round((((celTemp - 273.15) * 9) / 5) + 32) + "°";
-    
-      // 
+      // wind speed
+      var wind = (data.wind.speed);
+      // uv index
+      // var uvIndex = (data.)
+      var icon = (data.weather[0].icon);
       cityName.innerHTML = city;
       // where todays city/weather shows up in the large box
       var cityBox = document.querySelector(".city");
       // have city show up in citybox
-      cityBox.innerHTML = 'City: ' + city + ' ' + today + '</br>' + "Temperature: " + fTemp + '</br>' + 'Wind Speed: ' + '</br>' + "Humidity: " + humidity + '</br>' + 'UV Index: ';
+      cityBox.innerHTML = 'City: ' + city + ' ' + today + '</br>' + "Temperature: " + fTemp + '</br>' + 'Wind Speed: ' + wind + '</br>' + "Humidity: " + humidity + '</br>' + 'UV Index: ' ;
     });
     
 }
@@ -71,17 +75,19 @@ let boxDay = date.getDate();
 
 // check day so you get today and the next 4 days
 function CheckDay(day) {
-  if (day + date.getDay() > 6) {
-    return day + date.getDay() - 7;
+  if ((day + 1) + date.getDay() > 6) {
+    // add 1 to start on tomorrow
+    return (day+1) + date.getDay() - 7;
   } else {
     return day + date.getDay();
   }
 }
-
+// bottom 5 boxes
 function boxInfo() {
     // add days of week and date to boxes
-    for (let i = 0; i < 5; i++) {
-        document.getElementById("day" + (i + 1)).innerHTML = weekday[CheckDay(i)] + '</br>' +
+  for (let i = 0; i < 5; i++) { 
+      // the issue with every other box showing is on this line. Probably in (i + 1).----- weekday[CheckDay(i++)] add correct date
+        document.getElementById("day" + (1+i)).innerHTML = weekday[CheckDay(i++)] + '</br>' +
             (boxDay = '(' + (date.getMonth() + 1) + ' / ' + (date.getDate() + i++) + ' / ' + date.getFullYear() + ')') + '</br>' +
             'Temp: ' + '</br>' + `Wind Speed: ` + '</br>' + 'Humidity: ' + '</br>' + "UV Index: ";
     }
@@ -89,14 +95,14 @@ function boxInfo() {
 }
 boxInfo()
 
-for (i = 0; i < 5; i++) {
-  document.getElementById('day' + (i + 1) + 'Humidity').innerHTML = 'Humidity:' + Number(data.list[i].main.humidity);
-}
-for (i = 0; i < 5; i++) {
-  document.getElementById('day' + (i + 1) + 'Wind Speed').innerHTML = 'Wind Speed:' + Number(data.list[i].wind.speed);
-}
-for (i = 0; i < 5; i++) {
-  document.getElementsById("day" + (i + 1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
-}
+// for (i = 0; i < 5; i++) {
+//   document.getElementById('day' + (i + 1) + 'Humidity').innerHTML = 'Humidity:' + Number(data.list[i].main.humidity);
+// }
+// for (i = 0; i < 5; i++) {
+//   document.getElementById('day' + (i + 1) + 'Wind Speed').innerHTML = 'Wind Speed:' + Number(data.list[i].wind.speed);
+// }
+// for (i = 0; i < 5; i++) {
+//   document.getElementsById("day" + (i + 1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+// }
 
 
